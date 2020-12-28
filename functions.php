d862df924c121276c5dcc46c1c41728025067901
Daniel W. Robert <danielwrobert@gmail.com>
2020-12-28 14:16:12 -0500

    Fix color variable typo. Remove script enqueue.

--- functions.php
+++ functions.php
@@ -15,8 +15,5 @@ function dracula_publisher_2_scripts() {
 	// enqueue font styles
 	wp_enqueue_style( 'font-styles-muli', get_stylesheet_directory_uri() . '/assets/fonts/typeface-muli/index.css', array(), null );
 	wp_enqueue_style( 'font-styles-ovo', get_stylesheet_directory_uri() . '/assets/fonts/typeface-ovo/index.css', array(), null );
-
-	// enqueue custom scripts
-	wp_enqueue_script( 'dracula-publisher-2-scripts', get_stylesheet_directory_uri() . '/js/main.js', array(), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'dracula_publisher_2_scripts' );

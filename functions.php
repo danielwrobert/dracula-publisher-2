b5f1f3a537cf21023fbd78c8dc9e6b77d738c925
Daniel W. Robert <danielwrobert@gmail.com>
2020-12-29 09:46:34 -0500

    Remove comment link from post list meta.

--- functions.php
+++ functions.php
@@ -17,3 +17,56 @@ function dracula_publisher_2_scripts() {
 	wp_enqueue_style( 'font-styles-ovo', get_stylesheet_directory_uri() . '/assets/fonts/typeface-ovo/index.css', array(), null );
 }
 add_action( 'wp_enqueue_scripts', 'dracula_publisher_2_scripts' );
+
+
+/**
+ * Parent Theme Overrides
+ */
+
+/**
+ * Prints HTML with meta information for author and categories.
+ */
+function independent_publisher_2_entry_meta() { ?>
+		<div class="entry-meta">
+			<span class="byline">
+				<?php the_author_posts_link(); ?>
+			</span>
+			<?php if ( 'post' === get_post_type() ) { ?>
+				<span class="cat-links">
+					<?php
+						/* translators: Used between list items, there is a space after the comma */
+						echo get_the_category_list( esc_html__( ', ', 'independent-publisher-2' ) );
+					?>
+				</span><!-- .cat-links -->
+			<?php } ?>
+
+			<span class="published-on">
+				<?php if ( is_single() ) {
+					echo independent_publisher_2_posted_time();
+				} else { ?>
+					<a href="<?php the_permalink(); ?>" rel="bookmark"><?php echo independent_publisher_2_posted_time(); ?></a>
+				<?php } ?>
+			</span>
+
+			<?php
+
+			if ( independent_publisher_2_show_word_count() ) {
+				printf( '<span class="word-count">%s</span>', sprintf( _nx( '%s Minute', '%s Minutes', independent_publisher_2_word_count(), 'time to read', 'independent-publisher-2' ), independent_publisher_2_word_count() ) );
+			}
+
+			// Edit post link
+			if ( ! is_single() ) {
+				edit_post_link(
+					sprintf(
+						/* translators: %s: Name of current post */
+						esc_html__( 'Edit %s', 'independent-publisher-2' ),
+						the_title( '<span class="screen-reader-text">"', '"</span>', false )
+					),
+					'<span class="edit-link">',
+					'</span>'
+				);
+			} ?>
+		</div><!-- .entry-meta -->
+	<?php
+}
+
